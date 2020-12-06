576d7bec88faeb3eb09256f6ec90e8e018c2d6a1
Carlos Salazar <60816774+Commando20@users.noreply.github.com>
2020-12-05 21:44:43 -0500

    Recreated project by removing integrated bottom navigation bar and creating one manually. Added separate activities for switching between application and API was called and data was retrieved in main as a test.

---
+++ Final Project/app/src/main/java/com/example/finalproject/FavoritesActivity.java
@@ -0,0 +1,62 @@
+package com.example.finalproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FavoritesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView textView = findViewById(R.id.yelp);
+        textView.setText("333333333333333333333333333333");
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.navigation_favorites);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        Intent text = new Intent(FavoritesActivity.this, MainActivity.class);
+                        startActivity(text);
+                        break;
+                    case R.id.navigation_results:
+                        Intent map = new Intent(FavoritesActivity.this, ResultsActivity.class);
+                        startActivity(map);
+                        break;
+                    case R.id.navigation_favorites:
+                        Intent history = new Intent(FavoritesActivity.this, FavoritesActivity.class);
+                        startActivity(history);
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+
+}
--- Final Project/app/src/main/java/com/example/finalproject/MainActivity.java
+++ Final Project/app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.finalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.toolbox.StringRequest;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,13 +30,53 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder
-                (R.id.navigation_home, R.id.navigation_search, R.id.navigation_results, R.id.navigation_favorites).build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+
+        final TextView textView = findViewById(R.id.yelp);
+
+        StringRequest request = new StringRequest(Request.Method.GET, "https://api.yelp.com/v3/businesses/search?location=NYC", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                System.out.println(response);
+                textView.setText(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println(error);
+            }
+        }) {
+            //This is for Headers If You Needed
+            @Override
+            public Map<String,String> getHeaders() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("Authorization","bearer " + "IGeH9oQlcaQpbdrzxIUBCDfC6zgIC4dkRt2_LEE2W99GHrW5JKl91db_nWarHKP9RgpzfouaXn8IW3q8HEwF_o3V6tIzgghXQCWnKq5MGurm9vC7ZaJWkXD5yYyyX3Yx");
+                return params;
+            }
+        };
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        queue.add(request);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.navigation_home);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_search:
+                        Intent text = new Intent(MainActivity.this, SearchActivity.class);
+                        startActivity(text);
+                        break;
+                    case R.id.navigation_results:
+                        Intent map = new Intent(MainActivity.this, ResultsActivity.class);
+                        startActivity(map);
+                        break;
+                    case R.id.navigation_favorites:
+                        Intent history = new Intent(MainActivity.this, FavoritesActivity.class);
+                        startActivity(history);
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
